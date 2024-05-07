9d0d056828f985307a6988dd2c604d715ede4d51
warrenwilksjr <55510696+warrenwilksjr@users.noreply.github.com>
2024-05-07 09:26:56 -0700

    Add files via upload
    Split back movements to account for lower back loading
    Added user input for a more tailored routine

--- workout-source.py
+++ workout-source.py
@@ -31,11 +31,13 @@ rear_movement_rand = random.choice(rear_delt_movements)
 #Back Movements Definition
 vert_pull_movements = ["Neutral Lat Pulldown", "Pull ups","Assisted Pull-up", "Weighted Pull Ups", "Lat Pulldown","Single Arm Pulldown", "Single Arm Plate Loaded Pulldown",
                        "Close Grip Pulldown"]
-horzontal_row_movements = ["Barbell row", "Chest Supported Row", "Meadows Row", "T Bar Row", "Chest Supported T Bar Row", "Smith Machine Row","Dumbbell Row",
+horzontal_row_movements = [ "Chest Supported Row",   "Chest Supported T Bar Row", "Dumbbell Row",
                            "Chest Supported Dumbbell Row","Low Cable Row"]
-trap_movements = ["Dumbbell Shrug", "Barbell Shrug", "Smith Machine Shrug", "Deadlift"]
+horzaontal_row_movements_axel = ["Barbell row","Meadows Row", "T Bar Row","Smith Machine Row"]
+trap_movements = ["Dumbbell Shrug", "Barbell Shrug", "Smith Machine Shrug"]
 vert_pull_movement_rand = random.choice(vert_pull_movements)
 horzontal_row_movement_rand = random.choice(horzontal_row_movements)
+horzontal_row_movement_axel_rand = random.choice(horzaontal_row_movements_axel)
 trap_movement_rand = random.choice(trap_movements)
 
 #Quad Movements Definition
@@ -46,7 +48,7 @@ secondary_quad_movement_rand = random.choice(secondary_quad_movements)
 
 #Hamstring Movements Definition
 ham_curl_movements = ["Seated Ham Curl", "Lying Ham Curl", "Kneeling Ham Curl"]
-hip_hinge_movements = ["RDL", "Stiff Leg Deadlift", "Dumbbell RDL"]
+hip_hinge_movements = ["RDL", "Stiff Leg Deadlift", "Dumbbell RDL", "Deadlift"]
 ham_curl_movement_rand = random.choice(ham_curl_movements)
 hip_hinge_movement_rand = random.choice(hip_hinge_movements)
 
@@ -55,7 +57,7 @@ calf_movements = ["Standing Calves", "Seated Calves", "Leg Press Calves"]
 calf_movement_rand = random.choice(calf_movements)
 
 #Rep Range Definition
-main_rep_range = ["3 x 5-8","3 x 6-10","3 x 8-12"]
+main_rep_range = ["3 x 5-8","3 x 6-10","3 x 8-12", "3 x 6-8, 8-10, 10-12", "3 x 10-12, 8-10, 6-8"]
 secondary_rep_range = ["3 x 8-12", "3 x 10-15","3 x 15-20"]
 all_ranges = ["3 x 5-8","3 x 6-10","3 x 8-12","3 x 10-15","3 x 15-20"]
 fail_rep_range = "3 x fail"
@@ -63,24 +65,70 @@ main_rep_range_rand = random.choice(main_rep_range)
 secondary_rep_range_rand = random.choice(secondary_rep_range)
 all_ranges_rand = random.choice(all_ranges)
 
+#Intensity 
+intensity_techiques = [":Last Set - dropset", ":Last Set - double dropset", ":Last Set triple dropset", ":Every Set myo-rep match", ":Last Set: 3 rest pause sets"]
+superset = ["y","n"]
+intensity_techiques_rand = random.choice(intensity_techiques)
+superset_rand = random.choice(superset)
+
+#Obtaining User Input
+hamsting_variation = input("Do you want to hip hinge or Leg Curl: \n 1. Hip Hinge \n 2. Leg Curl \n\n")
+squat_variation = input("Can you barbell squat: \n 1. Yes \n 2. No \n\n")
+side_or_rear_delt_focus = input("Do you want to focus side or rear delts? \n 1. Side \n 2. Rear \n\n")
+
 
 #Your weekly workout
-print("Here is your weekly routine: \n")
+print("\nHere is your weekly routine: \n")
 
-#Leg Print Out
+#Leg Print Out: Need to remove a hamstring movement from day. Move Deads to hams. Combine ham movements. These are advance
 print("Day 1 routine: \n")
-print("1. ",ham_curl_movement_rand,secondary_rep_range_rand)
-print("2. ",hip_hinge_movement_rand,main_rep_range_rand)
-print("3. ",quad_movements_rand,main_rep_range_rand)
-print("4. ",secondary_quad_movement_rand,secondary_rep_range_rand)
-print("5. ",calf_movement_rand,all_ranges_rand)
+
+if hamsting_variation == "2":
+        print("1. ",ham_curl_movement_rand,secondary_rep_range_rand, intensity_techiques_rand)
+elif hamsting_variation == "1" and hip_hinge_movement_rand == "Dumbbell RDL":
+        print("1. ",hip_hinge_movement_rand,secondary_rep_range_rand,intensity_techiques_rand)
+else:
+        print("1. ",hip_hinge_movement_rand,main_rep_range_rand)
+
+if squat_variation == "1":
+        print("2. ",quad_movements_rand,main_rep_range_rand)
+else:
+        print("2. ", secondary_quad_movement_rand,secondary_rep_range_rand,intensity_techiques_rand)
+print("3. ",secondary_quad_movement_rand,secondary_rep_range_rand,intensity_techiques_rand)
+print("4. ",calf_movement_rand,all_ranges_rand,intensity_techiques_rand)
 
 
 print("\n")
 
+#Back and Shoulder Print. Pick between a side or rear delt. add both once advance
+print("Day 2 routine: \n")
+if side_or_rear_delt_focus == "1":
+        print("1. ",side_movement_rand,secondary_rep_range_rand,intensity_techiques_rand)
+else:
+     print("1. ",rear_movement_rand,secondary_rep_range_rand,intensity_techiques_rand)   
+print("2. ",front_movement_rand,main_rep_range_rand)
+
+if trap_movement_rand == "Dumbbell Shrug":
+        print("3. ",trap_movement_rand,secondary_rep_range_rand)
+else: print("3. ",trap_movement_rand,main_rep_range_rand)
+
+if vert_pull_movement_rand == "Pull ups" or vert_pull_movement_rand == "Assisted Pull-up":
+        print("4. ",vert_pull_movement_rand,fail_rep_range)
+elif vert_pull_movement_rand == "Weighted Pull Ups" or vert_pull_movement_rand == "Single Arm Plate Loaded Pulldown":
+        print("4. ",vert_pull_movement_rand,main_rep_range_rand)
+else: print("4. ",vert_pull_movement_rand,secondary_rep_range_rand)
+
+
+if hamsting_variation == "1" and horzontal_row_movement_rand == "Chest Supported Dumbbell Row" or horzontal_row_movement_rand == "Dumbbell Row" or  horzontal_row_movement_rand == "Low Cable Row":
+        print("5. ",horzontal_row_movement_rand,secondary_rep_range_rand)
+elif hamsting_variation == "2":
+        print("5. ",horzontal_row_movement_axel_rand,main_rep_range_rand)
+else: print("5. ",horzontal_row_movement_rand,main_rep_range_rand)
+
+print("\n")
 
 #Chest and Arms Print
-print("Day 2 routine: \n")
+print("Day 3 routine: \n")
 print("1. ",chest_movement_rand,main_rep_range_rand)
 print("2. ",fly_movement_rand,secondary_rep_range_rand)
 print("3. ",bicep_movement_rand,secondary_rep_range_rand)
@@ -88,26 +136,7 @@ print("4. ",tricep_movement_rand,secondary_rep_range_rand)
 
 print("\n")
 
-#Back and Shoulder Print Out
-print("Day 3 routine: \n")
-print("1. ",side_movement_rand,secondary_rep_range_rand)
-print("2. ",front_movement_rand,main_rep_range_rand)
-print("3. ",rear_movement_rand,secondary_rep_range_rand)
 
-if trap_movement_rand == "Dumbbell Shrug":
-        print("4. ",trap_movement_rand,secondary_rep_range_rand)
-else: print("4. ",trap_movement_rand,main_rep_range_rand)
-
-if vert_pull_movement_rand == "Pull ups" or vert_pull_movement_rand == "Assisted Pull-up":
-        print("5. ",vert_pull_movement_rand,fail_rep_range)
-elif vert_pull_movement_rand == "Weighted Pull Ups" or vert_pull_movement_rand == "Single Arm Plate Loaded Pulldown":
-        print("5. ",vert_pull_movement_rand,main_rep_range_rand)
-else: print("5. ",vert_pull_movement_rand,secondary_rep_range_rand)
-
-
-if horzontal_row_movement_rand == "Chest Supported Dumbbell Row" or horzontal_row_movement_rand == "Dumbbell Row" or  horzontal_row_movement_rand == "Low Cable Row":
-        print("6. ",horzontal_row_movement_rand,secondary_rep_range_rand)
-else: print("6. ",horzontal_row_movement_rand,main_rep_range_rand)
 
 
 
